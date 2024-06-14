5f645c05d4ebd3800a3f7f5016fcfdfbb49f2a3f
yoon <ge4621@naver.com>
2024-06-14 22:16:21 +0900

    MemberService Test
    MemberService Test

--- BootProject/src/main/java/com/yoon/project/service/MemberService.java
+++ BootProject/src/main/java/com/yoon/project/service/MemberService.java
@@ -9,7 +9,13 @@ import com.yoon.project.repository.MemoryMemberRepository;
 
 public class MemberService {
 
-	private final MemberRepository memberRepository = new MemoryMemberRepository();
+	//private final MemberRepository memberRepository = new MemoryMemberRepository();
+	
+	private final MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 	
 	/*
 	 * 회원 가입
--- BootProject/src/test/java/com/yoon/project/service/MemberServiceTest.java
+++ BootProject/src/test/java/com/yoon/project/service/MemberServiceTest.java
@@ -2,17 +2,38 @@ package com.yoon.project.service;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.yoon.project.domain.Member;
+import com.yoon.project.repository.MemberRepository;
+import com.yoon.project.repository.MemoryMemberRepository;
 
 public class MemberServiceTest {
 
-	MemberService memberService = new MemberService();
+	//MemberService memberService = new MemberService();
+	//MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+	
+	MemberService memberService;
+	MemoryMemberRepository memberRepository;
+	
+	@BeforeEach
+	public void beforeEach() {
+		memberRepository = new MemoryMemberRepository();
+		memberService = new MemberService(memberRepository);
+	}
+	
+	@AfterEach
+	public void afterEach() {
+		memberRepository.cleanStore();
+	}
 	
 	
 	@Test
@@ -33,8 +54,24 @@ public class MemberServiceTest {
 	@Test
 	public void 중복_회원_예외() {
 		//given
+		Member member1 = new Member();
+		member1.setName("spring");
 		
+
+		Member member2 = new Member();
+		member2.setName("spring");
 		//when
+		memberService.join(member1);
+		IllegalStateException e = assertThrows(IllegalStateException.class, ()->memberService.join(member2));
+		
+		assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+		
+//		try {
+//			memberService.join(member2);
+//			fail();
+//		}catch(IllegalStateException e){
+//			assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//		}
 		
 		//then
 	}
